04ac9ce6b297debd46dbc1f6392c88cf23475667
Daniel Bailey <danieljabailey@gmail.com>
2022-10-21 20:47:25 +0100

    hello world

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdint.h>
+#include "image.h"
+
+uint16_t calc_y(uint16_t y){
+    return ((y & 0x7) << 8) | ((y & 0x38) << 2) | ((y & 0xC0) << 5);
+}
 
 int main(){
     volatile uint8_t* bitmap = (0x4000);
-    for (uint16_t i = 0; i<= (192*32)-1; i++){
-        bitmap[i] = 0xAA;
+    for (uint16_t row = 0; row<= 5; row++){
+        uint16_t start = calc_y(row);
+        for (uint16_t i = 0; i<= 9; i++){
+            bitmap[start+i] = image[(row*10)+i];
+        }
     }
     while(1){ volatile asm ("nop"); }
 }
