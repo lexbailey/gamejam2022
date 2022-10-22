b7a66eee7528266794784f63035f59faa1175e7e
Daniel Bailey <danieljabailey@gmail.com>
2022-10-22 13:34:21 +0100

    optimising maze drawing

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
 #include <stdint.h>
-#include "image.h"
-
-/*
-uint16_t calc_y(uint16_t y){
-    return ((y & 0x7) << 8) | ((y & 0x38) << 2) | ((y & 0xC0) << 5);
-}
-*/
 
 #define CALC_Y(y) ((((y) & 0x7) << 8) | (((y) & 0x38) << 2) | (((y) & 0xC0) << 5))
 #define NTILES (2)
@@ -27,26 +20,55 @@ uint8_t tiles[NTILES*32] = {
 #define MAPH (50)
 #define MAPSZ (MAPH * MAPW)
 #define XY(x,y) ((x)+((y)*MAPW))
-#define TILE(n) (tiles + (32*(n)))
+#define TILE(n) (tiles + ((n)<<5))
 
 uint8_t maze[MAPSZ];
 
-uint16_t px;
-uint16_t py;
+uint16_t px = 5;
+uint16_t py = 5;
 
+/*
 void render_maze(){
     volatile uint8_t* bitmap = (uint8_t *)(0x4000);
     for (uint16_t y = 0; y <= 9; y++){
+        uint16_t my = y + py - 5;
         for (uint16_t x = 0; x <= 9; x++){
-            uint8_t *t = TILE(maze[XY(x,y)]);
-            for (uint16_t i = 0; i<= 7; i++){
-                uint16_t sy1 = CALC_Y((16*y)+i);
-                uint16_t sy2 = CALC_Y(((16*y)+8)+i);
-                bitmap[sy1+(x*2)]   = t[i*4];
-                bitmap[sy1+(x*2)+1] = t[(i*4)+1];
-                bitmap[sy2+(x*2)]   = t[(i*4)+2];
-                bitmap[sy2+(x*2)+1] = t[(i*4)+3];
+            uint8_t *t = TILE(maze[XY(x+px-5,my)]);
+            uint16_t sy = CALC_Y(y<<4);
+            uint16_t by = sy+(x<<1);
+            uint16_t *bp = (uint16_t *)(bitmap+by);
+            uint16_t *tp = (uint16_t *)(t);
+            for (uint16_t i = 0; i<= 28; i+=4){
+                *bp = *tp;
+                bp += 16;
+                tp += 1;
+
+                *bp = *tp;
+                tp += 1;
+                bp += 112;
             }
+        }
+    }
+}
+*/
+
+void render_maze(){
+    volatile uint8_t* bitmap = (uint8_t *)(0x4000);
+    for (uint16_t xy = 0; xy <= 63; xy++){
+        uint16_t my = (xy>>3) + py - 5;
+        uint8_t *t = TILE(maze[XY((xy&0x7)+px-5,my)]);
+        uint16_t sy = CALC_Y((xy&0xFFF8)<<1);
+        uint16_t by = sy+((xy&0x7)<<1);
+        uint16_t *bp = (uint16_t *)(bitmap+by);
+        uint16_t *tp = (uint16_t *)(t);
+        for (uint16_t i = 0; i<= 28; i+=4){
+            *bp = *tp;
+            bp += 16;
+            tp += 1;
+
+            *bp = *tp;
+            tp += 1;
+            bp += 112;
         }
     }
 }
@@ -63,15 +85,25 @@ void test_maze(){
     }
 }
 
+void init_ui(){
+    volatile uint8_t* attrs = (uint8_t *)(0x5800);
+    
+}
+
 int main(){
-    volatile uint8_t* bitmap = (uint8_t *)(0x4000);
-    for (uint16_t row = 0; row<= 5; row++){
-        uint16_t start = CALC_Y(row);
-        for (uint16_t i = 0; i<= 9; i++){
-            bitmap[start+i] = image[(row*10)+i];
+    // black border
+    asm(
+        "push af\n"
+        "ld a,0\n"
+        "call 8859\n"
+        "pop af\n"
+    );
+    test_maze();
+    while (1){
+        render_maze();
+        px += 1;
+        if (px > 15){
+            px = 5;
         }
     }
-    test_maze();
-    render_maze();
-    while(1){ /*volatile asm ("nop");*/ }
 }
